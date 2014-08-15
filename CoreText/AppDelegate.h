b00a4bc1e5652bb6f0429d3c02dab0ca87aa3d49
comeOnYMH <yangminghai@yangminghaideMacBook-Pro.local>
2014-08-14 22:17:19 -0700

    Initial Commit

---
+++ CoreText/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreText
+//
+//  Created by 杨名海 on 14-8-14.
+//  Copyright (c) 2014年 杨名海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreText/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreText
+//
+//  Created by 杨名海 on 14-8-14.
+//  Copyright (c) 2014年 杨名海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
